80acfbd43a033a134651935326be6d4e85a176a7
AlunesStudios <macablastbytk@gmail.com>
2025-05-27 22:01:13 +0530

    Update App.py

--- App.py
+++ App.py
@@ -11,6 +11,13 @@ BB = st.button("BB")
 HB = st.button("HB")
 FB = st.button("FB")
 
+Amount_of_Rooms = st.write("Amount of rooms")
+
+One = st.button("One")
+Two = st.button("Two")
+Three = st.button("Three")
+Four = st.button("Four")
+
 Number_of_Adults = (st.text_input("Amount of Adults(12&above)"))
 Number_of_Children_0_5 = (st.text_input("Amount of Children(0-5)"))
 Number_of_Children_6_11 = (st.text_input("Amount 0f Children(6-11)"))
